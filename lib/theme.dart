8ffe8d70f0b865f03c31d0ec61f78a734fb6909c
0940185778 <biniyamassefa648@gmail.com>
2024-10-18 11:27:15 +0300

    feat: updating some provider colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-ThemeData lightmode = ThemeData(
-  brightness: Brightness.light,
-)
-;
-ThemeData darkmode = ThemeData(
-  brightness: Brightness.dark,
-);
-
-
+ThemeData lightmode =
+    ThemeData(brightness: Brightness.light, primaryColor: Colors.black);
+ThemeData darkmode =
+    ThemeData(brightness: Brightness.dark, primaryColor: Colors.white);
